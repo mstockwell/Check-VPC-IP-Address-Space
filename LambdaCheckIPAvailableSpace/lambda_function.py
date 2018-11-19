9ffd8bd805e8c55714c09f7f06c0c2a23db5895d
EC2 Default User <ec2-user@ip-172-31-0-5.us-west-2.compute.internal>
2018-11-19 22:11:59 +0000

    checks for VPC

--- LambdaCheckIPAvailableSpace/lambda_function.py
+++ LambdaCheckIPAvailableSpace/lambda_function.py
@@ -4,34 +4,33 @@ import boto3
 import os
 import ipaddress
 
-
 ec2 = boto3.resource('ec2')
 notify = boto3.client('sns')
 
-vpc = ec2.Vpc(os.environ['VPC_ID'])
-percent_warning = int(os.environ['PERCENTAGE_WARNING'])
-target_arn = os.environ['TARGET_ARN']
-subject = os.environ['MESSAGE_SUBJECT']
-
 def compute_available_ips(subnet):
-    print('Subnet: ',subnet.id)
     available_ips = subnet.available_ip_address_count
-    print('Remaining IPs available: ', available_ips)
     total_ips = ipaddress.ip_network(subnet.cidr_block).num_addresses
-    print('Total IPs in the subnet: ', total_ips)
     return round(available_ips/total_ips,2)*100
     
 def lambda_handler(event, context):
-    subnets = list(vpc.subnets.all())
-    message_txt = ''
-    for subnet in subnets:
-        percent_remaining = compute_available_ips(subnet)
-        if percent_remaining <= percent_warning:
-            message_txt += 'Subnet: ' + subnet.id + ' has ' + str(percent_remaining) \
-            + '% remaining IP addresses available...' +'\r'
-    if message_txt:
-        notify.publish (
-        TargetArn=target_arn,
-        Subject=subject,
-        Message=(message_txt)
-        )
+    percent_warning = int(os.environ['PERCENTAGE_WARNING'])
+    target_arn = os.environ['TARGET_ARN']
+    subject = os.environ['MESSAGE_SUBJECT']
+    vpc = ec2.Vpc(os.environ['VPC_ID'])
+    
+    if not vpc.vpc_id:
+        print ('No VPC')
+    else:
+        subnets = list(vpc.subnets.all())
+        message_txt = ''
+        for subnet in subnets:
+            percent_remaining = compute_available_ips(subnet)
+            if percent_remaining <= percent_warning:
+                message_txt += 'Subnet: ' + subnet.id + ' has ' + str(percent_remaining) \
+                + '% remaining IP addresses available...' +'\r'
+        if message_txt:
+            notify.publish (
+            TargetArn=target_arn,
+            Subject=subject,
+            Message=(message_txt)
+            )
