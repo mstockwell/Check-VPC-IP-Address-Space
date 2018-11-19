d72e0ce0f37412a041f9a784b6376d35a7d4113f
EC2 Default User <ec2-user@ip-172-31-0-5.us-west-2.compute.internal>
2018-11-19 22:39:25 +0000

    Option for Single VPC or All VPCs in All Regions

--- LambdaCheckIPAvailableSpace/lambda_function.py
+++ LambdaCheckIPAvailableSpace/lambda_function.py
@@ -17,20 +17,35 @@ def lambda_handler(event, context):
     target_arn = os.environ['TARGET_ARN']
     subject = os.environ['MESSAGE_SUBJECT']
     vpc = ec2.Vpc(os.environ['VPC_ID'])
+    message_txt =''
     
     if not vpc.vpc_id:
-        print ('No VPC')
+        region_client = boto3.client('ec2')
+        regions = region_client.describe_regions()
+        for region in regions['Regions']:
+            vpc_client = boto3.client('ec2', region_name=region['RegionName'])
+            vpcs = vpc_client.describe_vpcs()
+            for vpc in vpcs['Vpcs']:
+                vpc_resource = boto3.resource('ec2',region_name=region['RegionName'])
+                vpc_object = vpc_resource.Vpc(vpc['VpcId'])
+                subnets = list(vpc_object.subnets.all())
+                for subnet in subnets:
+                    percent_remaining = compute_available_ips(subnet)
+                    if percent_remaining <= percent_warning:
+                        message_txt += 'Subnet: ' + subnet.id + ' in VPC ' + vpc_object.vpc_id + \
+                            ' in Region ' + region['RegionName'] + ' has ' + str(percent_remaining) \
+                            + '% remaining IP addresses available...' +'\r'
     else:
         subnets = list(vpc.subnets.all())
         message_txt = ''
         for subnet in subnets:
             percent_remaining = compute_available_ips(subnet)
             if percent_remaining <= percent_warning:
-                message_txt += 'Subnet: ' + subnet.id + ' has ' + str(percent_remaining) \
-                + '% remaining IP addresses available...' +'\r'
-        if message_txt:
-            notify.publish (
-            TargetArn=target_arn,
-            Subject=subject,
-            Message=(message_txt)
-            )
+                message_txt += 'Subnet: ' + subnet.id + ' in VPC ' + vpc.vpc_id + ' has ' + \
+                str(percent_remaining) + '% remaining IP addresses available...' +'\r'
+    if message_txt:
+        notify.publish (
+        TargetArn=target_arn,
+        Subject=subject,
+        Message=(message_txt)
+        )
